c84d53f908f286b394a526e067ddac0350568c60
coder187 <coder187@hotmail.com>
2021-07-29 10:06:08 +0000

    add about and index page

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
