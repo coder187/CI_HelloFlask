a94ffa25e09099560f08ea342f4b1f38d9b172be
coder187 <coder187@hotmail.com>
2021-07-29 23:04:13 +0000

    print form values to the terminal

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import os
 import json
 
@@ -33,8 +33,11 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
     
-@app.route("/contact")
+@app.route("/contact", methods =["GET", "POST"])
 def contact():
+    if request.method == "POST": 
+        print(request.form.get("name")) #will return "none" if key not found
+        print(request.form["email"]) #will raise exception if key not found
     return render_template("contact.html", page_title="Contact")
 
 
